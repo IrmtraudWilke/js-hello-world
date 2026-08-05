e6b1966556afbabb572d0b1c2a6bb4791daceb01
Irmtraud Wilke <IrmtraudWilke@proton.me>
2026-08-05 23:24:11 +0330

    Create Home component in index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, sans-serif",
+      fontSize: 24
+    }}>
+      Hello, world!
+    </main>
+  );
+}
